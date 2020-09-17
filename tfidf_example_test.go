d707a0df618dd6024c78f5d37286264b1d3cea91
chewxy <chewxy@gmail.com>
2020-09-17 10:16:21 +1000

    Added proper search query example

--- tfidf_example_test.go
+++ tfidf_example_test.go
@@ -2,9 +2,13 @@ package tfidf_test
 
 import (
 	"fmt"
+	"math"
+	"sort"
 	"strings"
 
 	. "github.com/go-nlp/tfidf"
+	"github.com/xtgo/set"
+	"gorgonia.org/tensor"
 )
 
 var mobydick = []string{
@@ -56,6 +60,131 @@ func makeDocuments(a []string, c map[string]int) []Document {
 	return retVal
 }
 
+type docScore struct {
+	id    int
+	score float64
+}
+
+type docScores []docScore
+
+func (ds docScores) Len() int           { return len(ds) }
+func (ds docScores) Less(i, j int) bool { return ds[i].score < ds[j].score }
+func (ds docScores) Swap(i, j int) {
+	ds[i].score, ds[j].score = ds[j].score, ds[i].score
+	ds[i].id, ds[j].id = ds[j].id, ds[i].id
+}
+
+func cosineSimilarity(queryScore []float64, docIDs []int, relVec []float64) docScores {
+	// special case
+	if len(docIDs) == 1 {
+		// even more special case!
+		if len(queryScore) == 1 {
+			return docScores{
+				{docIDs[0], queryScore[0] * relVec[0]},
+			}
+		}
+
+		q := tensor.New(tensor.WithBacking(queryScore))
+		m := tensor.New(tensor.WithBacking(relVec))
+		score, err := q.Inner(m)
+		if err != nil {
+			panic(err)
+		}
+		return docScores{
+			{docIDs[0], score.(float64)},
+		}
+	}
+
+	m := tensor.New(tensor.WithShape(len(docIDs), len(queryScore)), tensor.WithBacking(relVec))
+	q := tensor.New(tensor.WithShape(len(queryScore)), tensor.WithBacking(queryScore))
+	dp, err := m.MatVecMul(q)
+	if err != nil {
+		panic(err)
+	}
+
+	m2, err := tensor.Square(m)
+	if err != nil {
+		panic(err)
+	}
+
+	normDocs, err := tensor.Sum(m2, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	normDocs, err = tensor.Sqrt(normDocs)
+	if err != nil {
+		panic(err)
+	}
+
+	q2, err := tensor.Square(q)
+	if err != nil {
+		panic(err)
+	}
+	normQt, err := tensor.Sum(q2)
+	if err != nil {
+		panic(err)
+	}
+	normQ := normQt.Data().(float64)
+	normQ = math.Sqrt(normQ)
+
+	norms, err := tensor.Mul(normDocs, normQ)
+	if err != nil {
+		panic(err)
+	}
+
+	cosineSim, err := tensor.Div(dp, norms)
+	if err != nil {
+		panic(err)
+	}
+	csData := cosineSim.Data().([]float64)
+
+	var ds docScores
+	for i, id := range docIDs {
+		score := csData[i]
+		ds = append(ds, docScore{id: id, score: score})
+	}
+	return ds
+
+}
+
+func contains(query Document, in []Document, tf *TFIDF) (docIDs []int, relVec []float64) {
+	q := query.IDs()
+	q = set.Ints(q) // unique words only
+	for i := range in {
+		doc := in[i].IDs()
+
+		var count int
+		var relevant []float64
+		for _, wq := range q {
+		inner:
+			for _, wd := range doc {
+				if wq == wd {
+					count++
+					break inner
+				}
+			}
+		}
+		if count == len(q) {
+			// calculate the score of the doc
+			score := tf.Score(in[i])
+			// get the  scores of the relevant words
+			for _, wq := range q {
+			inner2:
+				for j, wd := range doc {
+					if wd == wq {
+						relevant = append(relevant, score[j])
+						break inner2
+					}
+				}
+			}
+			docIDs = append(docIDs, i)
+			relVec = append(relVec, relevant...)
+		}
+	}
+	return
+}
+
 func Example() {
 	corpus, invCorpus := makeCorpus(mobydick)
 	docs := makeDocuments(mobydick, corpus)
@@ -74,8 +203,40 @@ func Example() {
 		}
 	}
 
-	scores := tf.Score(docs[0])
-	fmt.Printf("Scores: %1.1f", scores)
+	// now we search
+
+	// "ishmael" is a query
+	ishmael := doc{corpus["ishmael"]}
+
+	// "whenever i find" is another query
+	whenever := doc{corpus["whenever"], corpus["i"], corpus["find"]}
+
+	// step1: score the queries
+	ishmaelScore := tf.Score(ishmael)
+	wheneverScore := tf.Score(whenever)
+
+	// step2: find the docs that contains the queries.
+	// if there are no docs, oops.
+	ishmaelDocs, ishmaelRelVec := contains(ishmael, docs, tf)
+	wheneverDocs, wheneverRelVec := contains(whenever, docs, tf)
+
+	// step3: calculate the cosine similarity
+	ishmaelRes := cosineSimilarity(ishmaelScore, ishmaelDocs, ishmaelRelVec)
+	wheneverRes := cosineSimilarity(wheneverScore, wheneverDocs, wheneverRelVec)
+
+	// step4: sort the results
+	sort.Sort(sort.Reverse(ishmaelRes))
+	sort.Sort(sort.Reverse(wheneverRes))
+
+	fmt.Printf("Relevant Docs to \"Ishmael\":\n")
+	for _, d := range ishmaelRes {
+		fmt.Printf("\tID   : %d\n\tScore: %1.3f\n\tDoc  : %q", d.id, d.score, mobydick[d.id])
+	}
+	fmt.Println("")
+	fmt.Printf("Relevant Docs to \"whenever i find\":\n")
+	for _, d := range wheneverRes {
+		fmt.Printf("\tID   : %d\n\tScore: %1.3f\n\tDoc  : %q", d.id, d.score, mobydick[d.id])
+	}
 	// Output:
 	// IDF:
 	// 	"call": 2.4
@@ -89,5 +250,15 @@ func Example() {
 	// 	"never": 2.4
 	// 	"mind": 2.4
 	// 	"how": 2.4
-	// Scores: [0.6 0.3 0.6 0.1]
+	// Relevant Docs to "Ishmael":
+	//	ID   : 0
+	//	Score: 1.437
+	//	Doc  : "Call me Ishmael ."
+	// Relevant Docs to "whenever i find":
+	//	ID   : 5
+	//	Score: 0.985
+	//	Doc  : "whenever I find myself involuntarily pausing before coffin warehouses , and bringing up the rear of every funeral I meet ; "	ID   : 3
+	//	Score: 0.962
+	//	Doc  : "Whenever I find myself growing grim about the mouth ; "
+
 }
